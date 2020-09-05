ed316fedb1e5622b8ecbe7db4cfc479d36d9874d
rotariuBogdan <b.rotariu@sologics.de>
2020-09-05 20:33:40 +0300

    css workshop init

---
+++ src/examples/basic/Basic.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Basic.styles.scss'
+
+export const BasicGrid = () => {
+    return (
+        <div className="grid-container">
+        <div className="grid-item">item 1</div>
+        <div className="grid-item">item 2</div>
+        <div className="grid-item">item 3</div>
+        <div className="grid-item">item 4</div>
+        <div className="grid-item">item 5</div>
+        <div className="grid-item">item 6</div>
+        <div className="grid-item">item 7</div>
+        <div className="grid-item">item 8</div>
+        <div className="grid-item">item 9</div>
+        <div className="grid-item">item 10</div>
+      </div>
+    )
+}
