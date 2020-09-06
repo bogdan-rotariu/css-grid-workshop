040700fd55797e5bf6c754082ea01a25713aea00
rotariuBogdan <b.rotariu@sologics.de>
2020-09-06 17:23:31 +0300

    bugfix

--- src/examples/06-template-areas-grid/TemplateAreasGrid.jsx
+++ src/examples/06-template-areas-grid/TemplateAreasGrid.jsx
@@ -3,14 +3,5 @@ import { BasicGrid } from '../01-basic/Basic'
 import './TemplateAreasGrid.styles.scss'
 
 export const TemplateAreasGrid = () => {
-    const styles = {
-        gridTemplateAreas: 
-            `
-            "left center right"
-            "left center right"
-            "bottom bottom bottom"
-            `,
-        gridTemplateColumns: '100px 1fr 100px',
-    }
-    return <BasicGrid style={styles} className='template-areas-grid' />
+    return <BasicGrid className='template-areas-grid' />
 }
